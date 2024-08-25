ec9e197e9554583fdfe1d95145fd4e602c77b47f
Himanshu Masani <47810143+masani1989@users.noreply.github.com>
2024-08-25 06:13:08 +0530

    tests

--- views/about_me.py
+++ views/about_me.py
@@ -46,7 +46,8 @@ with rbtn:
         with st.spinner('In-Progress......'):
             st.cache_data.clear()
             now = pd.DataFrame({'DataAsOf': [(datetime.utcnow() + timedelta(minutes=330)).strftime("%Y-%m-%d %H:%M:%S")]})
-            rd.refGw()
+            x = rd.refGw()
+            st.write(x)
             gs.update_data(wksheet='DataDate', df=now)
 
 # Section to display the latest date for which the data is present in the app
