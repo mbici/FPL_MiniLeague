a3cedf218fcfc30a746c0ca618f3a5cda183dc8f
Himanshu Masani <47810143+masani1989@users.noreply.github.com>
2024-08-25 06:10:18 +0530

    tests

--- Utils/refreshData.py
+++ Utils/refreshData.py
@@ -1,6 +1,7 @@
 import Utils.gsheet_conn as gs
 from Utils.league import *
 import Utils.gameweek as gwk
+import streamlit as st
 
 lg = league(140708)
 currGw = gwk.get_recent_completed_gameweek()
@@ -50,6 +51,7 @@ def refGw():
     Function to refresh the latest ongoing/completed gameweek's data
     :return:
     """
+    st.write(currGw[0])
     plList = lg.get_league_players()
     delete_rows_based_on_column('FPL_Fantasy_Kings', 'Gameweek', 7, f'{currGw[0]}')
     gw_plr_list = []
